fe75390d617f4076a87c39920507173e1dafeaa4
Nichlas Pilemand <nnpilemand@gmail.com>
2016-03-29 11:42:26 +0200

    Added basic fields

---
+++ Matador_3/src/fields/Ownable.java
@@ -0,0 +1,37 @@
+package fields;
+
+/**
+ * @author Nichlas N. Pilemand
+ *
+ */
+public class Ownable extends Field {
+
+	protected int price;
+	protected Player owner;
+	
+	public Ownable(int id, String name, int price) {
+		super(id, name);
+		this.price = price;
+	}
+	
+	
+	/**
+	 * @return true if owned, otherwise false
+	 */
+	public boolean isOwned() {
+		return (owner == null) ? false : true;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public Player getOwner(){
+		return owner;
+	}
+	
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+}
+
