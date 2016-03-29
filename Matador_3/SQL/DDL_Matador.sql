33c5ebe4016000037e00e7c63e15d0f2846bc952
Nichlas Pilemand <nnpilemand@gmail.com>
2016-03-29 14:53:09 +0200

    DDL update

--- Matador_3/SQL/DDL_Matador.sql
+++ Matador_3/SQL/DDL_Matador.sql
@@ -7,21 +7,9 @@ drop table if exists player_properties;
 drop table if exists bank;
 drop table if exists jail;
 
-create table players(
-	id 			int(6),
-    game_id		int(1000),
-    name 		varchar(20),
-    account		int(1000000),
-    jailcards 	int(2),
-	color		varchar(10),
-    position	int(40),
-    primary key(id, game_id),
-    foreign key (game_id) references games(id)
-		on delete set null
-);
 
 create table games(
-	id 					int(1000),
+	id 					int AUTO_INCREMENT not null,
     game_name			varchar(30),
     last_saved_time		timestamp,
     current_player		int(6),
@@ -29,36 +17,46 @@ create table games(
     primary key(id)
 );
 
-create table player_properties(
-	field_id	int(40),
-    num_houses	int(4),
-    num_hotels	int(1),
-    field_type	varchar(10),
-    player_id	int(6),
-    game_id		int(1000),
-    primary key (field_id),
-	foreign key (player_id) references players(id)
-		on delete set null,
+create table players(
+	id 			int,
+    game_id		int,
+    player_name	varchar(20),
+    account 	int,
+    jailcards 	int,
+	color		varchar(10),
+    position	int,
+    primary key(id, game_id),
     foreign key (game_id) references games(id)
-		on delete set null
+);
+
+create table player_properties(
+	field_id	int,
+    num_houses	int,
+    num_hotels	int,
+    field_type	varchar(10),
+    player_id	int,
+    game_id		int,
+    primary key (field_id),
+	foreign key (player_id) references players(id),
+    foreign key (game_id) references games(id)
 
 );
 
 create table bank(
-	houses_left		int(32),
-    hotels_left		int(12),
-    game_id			int(1000),
+	houses_left		int,
+    hotels_left		int,
+    game_id			int,
     primary key(game_id),
-    foreign key(game_id) references games(id) on delete set null
+    foreign key(game_id) references games(id)
 );
 
 create table jail(
-	player_id		int(6),
-    game_id			int(1000),
-    num_doubles		int(3),
-    jail_time		int(3),
+	player_id		int,
+    game_id			int,
+    num_doubles		int,
+    jail_time		int,
     isInJail		boolean,
     primary key (player_id, game_id),
-    foreign key (player_id) references players(id) on delete set null,
-    foreign key (game_id) references games(id) on delete set null
+    foreign key (player_id) references players(id),
+    foreign key (game_id) references games(id)
 );
