1374a999401ff6f51f56d217e74abc2db0f0e3dc
Nichlas Pilemand <nnpilemand@gmail.com>
2016-04-18 20:48:20 +0200

    Added Chance field

---
+++ Matador_3/src/fields/Chance.java
@@ -0,0 +1,32 @@
+/**
+ * The NonOwnable Field, Chance. Several of these exists in a standard Monopoly game.
+ * @author Nichlas N. Pilemand
+ */
+package fields;
+
+import desktop_codebehind.Player;
+
+public class Chance extends AbstractNonOwnables {
+	
+	private static final String FIELD_TYPE = "Chance";
+
+	/**
+	 * Creates a NonOwnable Field of type Chance. Used to give random cards
+	 * to players during a game.
+	 */
+	public Chance() {
+		// Name and fieldType is the same.
+		super(FIELD_TYPE, FIELD_TYPE);
+	}
+
+	/**
+	 * Draws and executes a chance card on the player landing on the Field.
+	 * @param player The player landing on the Field.
+	 */
+	@Override
+	public void landOnField(Player player) {
+		// TODO Draw and execute a chance card on the player
+
+	}
+
+}
