6d2551070ab64d07bfc30eaf5bb1e86d1e25df30
Henrik <Henrik@Henrik-pc.hhk.dk>
2016-04-18 11:37:28 +0200

    StringtoString

--- Matador_3/src/fields/AbstractOwnable.java
+++ Matador_3/src/fields/AbstractOwnable.java
@@ -56,5 +56,14 @@ public abstract class AbstractOwnable extends AbstractFields {
 	public void setOwner(Player owner) {
 		this.owner = owner;
 	}
+	
+	@Override
+	public String toString(){
+		String str = super.toString() + ", price=" + price;
+		if(owner != null)
+			str += ", owner=" + owner.getName();
+		
+		return str;
+	}	
 }
 
