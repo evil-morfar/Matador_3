2ae1fd0dbf3ff2f0649900968e0416565031e495
Nichlas Pilemand <nnpilemand@gmail.com>
2016-04-18 20:54:34 +0200

    Added FlatTax field

---
+++ Matador_3/src/fields/FlatTax.java
@@ -0,0 +1,34 @@
+/**
+ * The NonOwnable Field, FlatTax. There's only 1 of these on a standard Monopoly game.
+ * @author Nichlas N. Pilemand
+ */
+package fields;
+
+import desktop_codebehind.Player;
+
+public class FlatTax extends AbstractNonOwnables {
+	
+	private static final String FIELD_TYPE = "Tax";
+	private int amount;
+
+	/**
+	 * Creates an object of the FlatTax type. The tax to be payed is a flat amount.
+	 * @param name The name of the Field.
+	 * @param amount The amount to pay.
+	 */
+	public FlatTax(String name, int amount) {
+		super(name, FIELD_TYPE);
+		this.amount = amount;
+	}
+
+	@Override
+	/**
+	 * Forces the player landing on the field to pay the amount in taxes.
+	 * @param player The player landing on the field.
+	 */
+	public void landOnField(Player player) {
+		player.changeBalance(-this.amount);
+		//TODO consider error handling.
+	}
+
+}
