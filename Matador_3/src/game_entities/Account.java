b9e7f082d837f9729933b0548faa416b8f58875b
PC <PC@192.168.2.105>
2016-04-19 10:36:27 +0200

    Account methods and logic

--- Matador_3/src/game_entities/Account.java
+++ Matador_3/src/game_entities/Account.java
@@ -2,4 +2,28 @@ package game_entities;
 
 public class Account {
 
+	private int balance;
+	
+	public Account (int startingBalance){
+		this.balance = startingBalance; 
+		
+	}
+	
+	
+	public  int getBalance(){
+		return balance;
+	}
+	
+	public void deposit(int value){
+	
+		balance = balance + value;
+	}
+
+	public void withdraw(int value){
+		
+		balance = balance - value;
+		
+	}
+	
+	
 }
