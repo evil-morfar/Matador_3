7227b250577d8aae357f280447cde1ab4c59194d
Nichlas Pilemand <nnpilemand@gmail.com>
2016-04-19 11:15:33 +0200

    Moar BoardTests

--- Matador_3/src/tests/BoardTest.java
+++ Matador_3/src/tests/BoardTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import fields.AbstractFields;
+import fields.Territory;
 import game_entities.Board;
 
 /**
@@ -15,17 +17,22 @@ import game_entities.Board;
  *
  */
 public class BoardTest {
+	private AbstractFields[] fields;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		Board b = new Board();
+		AbstractFields[] fields = b.getFields();
 	}
 
 	@Test
-	public void test() {
-		Board b = new Board();
+	public void testFields() {
+		assert(fields[1].getName().equals("Start"));
+		assert(fields[40].getName().equals("Rådhuspladsen"));
+		assert(((Territory) fields[2]).getPrice() == 1200);
 	}
 
 }
