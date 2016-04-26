aa1d941a06651bfb031ba6999571fd5d20fb23ce
Henrik <Henrik@10.16.175.187>
2016-04-26 09:17:31 +0200

    Lavet reader

--- Matador_3/src/tests/ReaderTest.java
+++ Matador_3/src/tests/ReaderTest.java
@@ -1,39 +1,52 @@
+package tests;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ReaderTest {
-
 	
-	
+private static final String GUI_FILE = "src/game_boundaries/GUItext.csv";
 	
   public static void main(String[] args) {
     // TODO Auto-generated method stub/ indlæs fil med tekster
 	  
-	  String[] testie;
-		testie = new String[100];
-		int i = 0;
-	  
-	  
-    File file = new File("src/game_boundaries/GUItext.csv");
+	  String[] GUIline;
+		GUIline = new String[40];
 
-    try {
-
-      Scanner scanner = new Scanner(file);
-      
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        System.out.println(line);
-        testie[i] = line;
-        i++;
-        
-      }
-      scanner.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-
-
+		String line = "";
+		String splitBy = "#";
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(GUI_FILE));
+			int i = 0;
+			while((line = br.readLine()) != null) {
+				String[] field = line.split(splitBy);
+				
+				 GUIline[i] = field[0];
+				i++;
+			}
+		
+			System.out.println(GUIline[0]);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} finally {
+			if(br != null)
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
   }
 
+}
 }
