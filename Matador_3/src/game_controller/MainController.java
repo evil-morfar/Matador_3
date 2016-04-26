aeac4e185e55487fbf82b92795891bbd40839b49
Nichlas Pilemand <nnpilemand@gmail.com>
2016-04-26 10:28:24 +0200

    Added some helper methods

--- Matador_3/src/game_controller/MainController.java
+++ Matador_3/src/game_controller/MainController.java
@@ -1,6 +1,8 @@
 package game_controller;
 
 import game_entities.Board;
+import game_entities.Player;
+
 import java.util.ArrayList;
 import die_classes.DieCup;
 import die_classes.DieCup_Interface;
@@ -17,24 +19,36 @@ public class MainController {
 	//evt add flerer sub state under PLAY_STATE
 	public enum GameState{NAME_STATE, PLAY_STATE, WIN_STATE}; 
 	private GameState state = GameState.NAME_STATE;
-
-	
 	
 	private int turnNumber;
 	
-	private DieCup_Interface dieCup; 
+	private DieCup dieCup; 
 	private Interface output;
-
-	private ArrayList<String> playerNames;
+	private ArrayList<Player> players;
+	private Player currentPlayer;
+	private Board board;
 	
 	
 	//Constructor
 	public MainController(){
-		dieCup = DiceCup;
-		playerNames = new ArrayList<String>();
+		dieCup = new DieCup();
+		players = new ArrayList<Player>();
 		output = new GUI_boundary("");
+		board = new Board();
 	}
 	
+
+	public Player getCurrentPlayer() {
+		return currentPlayer;
+	}
+	
+	public Board getBoard() {
+		return board;
+	}
+	
+	public int getRoll(){
+		return dieCup.getSum();
+	}
 
 	public void run(){
 
