6291c67aa955927ae126d8c234b24ef2b9981ea3
Nichlas Pilemand <nnpilemand@gmail.com>
2016-05-11 15:49:58 +0200

    More DB tests

--- Matador_3/src/tests/DatabaseTest.java
+++ Matador_3/src/tests/DatabaseTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -10,14 +13,14 @@ import org.junit.runners.MethodSorters;
 import game.database.DatabaseAccess1;
 import game.entities.Board;
 import game.entities.Player;
-import game.entities.fields.AbstractOwnable;
+import game.entities.fields.Territory;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DatabaseTest {
 	
 	private static DatabaseAccess1 db;
 	private static boolean hasCon;
-	private int game_id;
+	private static int game_id; //Apparently has to be static
 	private static Board board;
 	
 	@BeforeClass
@@ -30,51 +33,72 @@ public class DatabaseTest {
 	}
 		
 	@Test
-	public void testCreateGame(){
-		if (hasCon) 
-			game_id = db.createNewGame("New Test Game", 1);
+	public void test1CreateGame(){
+		if (hasCon) {
+			DatabaseTest.game_id = db.createNewGame("New Test Game", 1);
+			System.out.println("Created game with id: "+game_id);
+			db.setGameID(game_id);
+			assertEquals(1, db.getCurrentPlayer());
+			assertTrue(db.getSavedGames().contains("New Test Game"));
+		}
 		
 	}
 	
-	@Test 
-	public void testSavePlayer(){
-		if(hasCon) {
-			Player p = new Player("Test player", 20000, "blue", 1, 1);
-			db.setGameID(game_id);
-			db.savePlayer(p);
-		}
-	}
-
 	@Test
 	public void testAddPlayer() {
 		if(hasCon) {
-			game_id = db.createNewGame("New Test Game", 1);
 			db.setGameID(game_id);
 			db.addNewPlayer(1, "testName", 30000, 0, "green", 1);
 		}
 	}
 	
+	@Test 
+	public void testSavePlayer(){
+		if(hasCon) {
+			db.setGameID(game_id);
+			// Create a player and add it to the db
+			Player p = new Player("Test player", 20000, "blue", 1, 2);
+			db.addNewPlayer(2, "Test player", 20000, 0, "blue", 1);
+			//Change a value
+			p.withdrawBalance(1000);
+			//p.getBalance() is now 19000, so save the player:
+			db.savePlayer(p);
+			ArrayList<Player> players = db.getPlayersFromGame(game_id);
+			//Getting the player from the DB give equal values to the original player
+			// (but of course it's not exactly the same, as it's a new object!)
+			Player p2 = players.get(1);
+			assertEquals(p.getBalance(), p2.getBalance());
+			assertEquals(p.getName(), p2.getName());
+			assertEquals(p.getColor(), p2.getColor());
+		}
+	}
+
+	
+	
 	@Test
 	public void testSaveGame(){
-		db.setGameID(1);
-		db.saveGame(1, board);
+		db.saveGame(2, board);
+		assertEquals(2, db.getCurrentPlayer());
 	}
 
 	@Test
 	public void testSaveField(){
 		if(hasCon){
-			game_id = db.createNewGame("Another Test game", 1);
 			db.setGameID(game_id);
-			db.saveField((AbstractOwnable) board.getFields()[1]);
-			
+			Territory field =  (Territory) board.getFields()[1];
+			field.setNumHouses(1);
+			db.saveField(field);
+			List<List<Integer>> fieldList = db.getFieldsFromGame(game_id);
+			assertEquals(field.getFieldID(), (int)(fieldList.get(0)).get(0));
+			assertEquals(field.getNumHouses(), (int)fieldList.get(0).get(2));			
 		}
 	}
 	
 	@Test
 	public void testBank(){
 		if(hasCon){
-			game_id = db.createNewGame("Yet Another Test game", 1);
 			db.setGameID(game_id);
+			//The DB inits the game with 32 houses and 12 hotels
 			assertEquals(32, db.getNumHousesLeft());
 			assertEquals(12, db.getNumHotelsLeft());
 		}
