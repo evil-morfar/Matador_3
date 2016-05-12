e561107831687080429804b369d766c1f369c5fc
Nichlas Pilemand <nnpilemand@gmail.com>
2016-05-12 19:01:38 +0200

    DML update

--- Matador_3/SQL/DML_Matador.sql
+++ Matador_3/SQL/DML_Matador.sql
@@ -3,7 +3,7 @@
 -- Create fictive game no.1
 call CreateGame("Medium Game Test", 1, @id);
 -- With 3 players, at various positions and states
-insert into players values(1,@id, "Player one", 21750, 0, "blue", 32);
+insert into players values(1,@id, "Player one", 14000, 0, "blue", 2);
 insert into jail values(1,@id, 0,0,false);
 insert into players values(2,@id, "Player two", 24000, 0, "white", 25);
 insert into jail values(2,@id,0,0,false);
